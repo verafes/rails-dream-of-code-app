1ee1b67ffe704bfa3d8f500120f7c4bf48717b8c
verafes <verafes@gmail.com>
2025-07-28 23:33:57 -0700

    Add Submission table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_01_191924) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_29_000814) do
   create_table "coding_classes", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -73,6 +73,20 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_01_191924) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "submissions", force: :cascade do |t|
+    t.integer "enrollment_id"
+    t.integer "lesson_id"
+    t.integer "mentor_id"
+    t.string "pull_request_url"
+    t.string "review_result"
+    t.datetime "reviewed_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["enrollment_id"], name: "index_submissions_on_enrollment_id"
+    t.index ["lesson_id"], name: "index_submissions_on_lesson_id"
+    t.index ["mentor_id"], name: "index_submissions_on_mentor_id"
   end
 
   create_table "trimesters", force: :cascade do |t|
