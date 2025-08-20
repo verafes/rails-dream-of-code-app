0bc7e5d00583103494e25a019884eedb7a43284b
verafes <verafes@gmail.com>
2025-08-19 21:47:47 -0700

    Add TrimestersController, views, tests

---
+++ spec/requests/trimesters_controller_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe "Trimesters", type: :request do
+  describe "GET /trimesters" do
+    context 'trimesters exist' do
+      before do
+        (1..2).each do |i|
+          Trimester.create!(
+            term: "Term #{i}",
+            year: '2025',
+            start_date: '2025-01-01',
+            end_date: '2025-01-01',
+            application_deadline: '2025-01-01',
+          )
+        end
+      end
+
+      it 'returns a page containing names of all trimesters' do
+        get '/trimesters'
+        expect(response.body).to include('Term 1 2025')
+        expect(response.body).to include('Term 2 2025')
+      end
+    end
+  end
+
+  describe "GET /trimesters/:id" do
+    let!(:trimester) do
+      Trimester.create!(
+        term: "Spring",
+        year: "2026",
+        start_date: "2026-01-01",
+        end_date: "2026-04-01",
+        application_deadline: "2025-11-01"
+      )
+    end
+
+    it "displays the trimester term and year on the show page" do
+      get "/trimesters/#{trimester.id}"
+      expect(response.body).to include("Spring")
+      expect(response.body).to include("2026")
+    end
+  end
+end
