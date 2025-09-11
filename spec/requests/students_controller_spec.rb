5f3810816359629a534287a7534b096aba83d9aa
verafes <verafes@gmail.com>
2025-09-10 22:07:47 -0700

    test added to students_controller_spec

--- spec/requests/students_controller_spec.rb
+++ spec/requests/students_controller_spec.rb
@@ -30,4 +30,20 @@ RSpec.describe "Students", type: :request do
       expect(response.body).to include('kj@test.com')
     end
   end
+
+  describe "GET /students/sorted_students" do
+    let!(:student1) { Student.create!(first_name: "KJ", last_name: "Loving", email: "kj@test.com") }
+    let!(:student2) { Student.create!(first_name: "AJ", last_name: "Suning", email: "aj@test.com") }
+    let!(:student3) { Student.create!(first_name: "Mila", last_name: "Johnson", email: "mila@test.com") }
+
+    it "returns students sorted alphabetically by last name then first name" do
+      get '/students/sorted_students'
+      expect(response).to have_http_status(:ok)
+
+      expect(response.body.index('Johnson')).to be < response.body.index('Loving')
+      expect(response.body.index('Loving')).to be < response.body.index('Suning')
+      expect(response.body.index('Mila')).to be < response.body.index('KJ')
+      expect(response.body.index('KJ')).to be < response.body.index('AJ')
+    end
+  end
 end
