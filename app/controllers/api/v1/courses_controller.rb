2082c952d3ec6cbe9ed0c92733ddb419a1f964d1
verafes <verafes@gmail.com>
2025-10-14 21:09:06 -0700

    fix controler and tests

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -1,17 +1,21 @@
 class Api::V1::CoursesController < ApplicationController
 
   def index
-    courses = Course.all
-    render json: {
-      courses: courses.map do |course|
-        {
-          id: course.id,
-          title: course.coding_class.title || '',
-          application_deadline: course.trimester&.application_deadline,
-          start_date: course.trimester&.start_date,
-          end_date: course.trimester&.end_date
-        }
-      end
-    }
+    current_trimester = Trimester.find_by("start_date <= ? AND end_date >= ?", Date.today, Date.today)
+    if current_trimester
+      courses_arr = current_trimester.courses
+      courses_hash = {
+        courses: courses_arr.map do |course|
+          {
+            id: course.id,
+            title: course.coding_class.title || '',
+            application_deadline: course.trimester&.application_deadline,
+            start_date: course.trimester&.start_date,
+            end_date: course.trimester&.end_date
+          }
+        end
+      }
+    render json: courses_hash, status: :ok
+    end
   end
 end
